0e048dd55d77966690e891a52d222aa75e58b2e0
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-12-03 12:49:42 +0100

    fix: bug import react

--- src/__tests__/SimpleModal.spec.tsx
+++ src/__tests__/SimpleModal.spec.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import {Text} from 'react-native';
 import {render} from '@testing-library/react-native';
 import {SimpleModal} from '../SimpleModal';
